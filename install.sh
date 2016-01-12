d1c412b3aba0bf278a3b89d643ac7e849c17f13f
Tom Wu <tomorzi@gmail.com>
2016-01-12 10:25:27 +0800

    新增一鍵安裝的sh執行檔

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+VIMHOME=~/.vim
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+[ -e "$VIMHOME/vimrc" ] && die "$VIMHOME/vimrc already exists."
+[ -e "~/.vim" ] && die "~/.vim already exists."
+[ -e "~/.vimrc" ] && die "~/.vimrc already exists."
+
+git clone git://github.com/tombear/vimrc.git "$VIMHOME"
+cd "$VIMHOME"
+git submodule update --init
+
+cd ~
+ln -s .vim/.vimrc .vimrc
+
+echo "Your VIM configuration has been installed."
